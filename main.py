a9e17a99ae98db248d3ad385236b3376473bcc67
Xury Sáez <estadistica2025cdce@gmail.com>
2026-04-01 11:47:59 -1100

    main.py

--- main.py
+++ main.py
@@ -116,20 +116,20 @@ u_data = st.session_state["user_data"]
 rol_usuario = str(u_data.get("rol", "")).lower()
 
 c_user, c_logo2, c_logo, c_mes = st.columns([1, 1, 2, 1])
-    with c_user:
+with c_user:
         col_btn1, col_btn2 = st.columns(2)
         if col_btn1.button("⚙️ Perfil"): ventana_configuracion()
         if col_btn2.button("🚪 Salir"): 
-    with c_logo:
+with c_logo:
         if os.path.exists("static/mppe.png"):
             st.image("static/mppe.png", width=100)
-    else:
-        st.markdown("<h3 style='text-align:center; color:#002D57;'>CDCE RIBAS</h3>", unsafe_allow_html=True)
+        else:
+            st.markdown("<h3 style='text-align:center; color:#002D57;'>CDCE RIBAS</h3>", unsafe_allow_html=True)
     with c_mes:
         meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
         mes_sel = st.selectbox("📅 Período:", meses, index=datetime.now().month-1)
-        st.session_state.clear()
-        st.rerun()
+            st.session_state.clear()
+            st.rerun()
 
 st.markdown("<h2 class='main-title'>Sistema Integrado de Estadísticas</h2>", unsafe_allow_html=True)
 modo = st.pills("Acción:", ["📊 Consultar", "📥 Cargar Datos"], default="📊 Consultar") if rol_usuario in ["admin", "director"] else "📊 Consultar"
