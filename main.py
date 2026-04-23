6527cdc253cb9eebe241f5ddb828130632c2eced
Xury Sáez <estadistica2025cdce@gmail.com>
2026-04-23 03:18:41 -1100

    main.py

--- main.py
+++ main.py
@@ -217,8 +217,8 @@ else:
                             try:
                                 total_inscritos = v_in + h_in
                                 total_asistencia = v_as + h_as
-                           if total_inscritos > 0:
-                           if total_asistencia > total_inscritos:
+                        if total_inscritos > 0:
+                        if total_asistencia > total_inscritos:
                                st.error(f"⚠️ **Error de Congruencia:** La asistencia total ({total_asistencia}) no puede ser mayor a la matrícula inscrita ({total_inscritos}). Por favor, corrija los valores.")
                            else: 
                             p_real = ((v_as + h_as) / total_inscritos) * 100
