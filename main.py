949dbf27665d6bc327ea497ff32c822475bdda89
Xury Sáez <estadistica2025cdce@gmail.com>
2026-06-25 11:24:30 -1100

    main.py

--- main.py
+++ main.py
@@ -184,6 +184,7 @@ else:
         meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
                  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
         # El selector ahora es independiente y funciona para todos los roles
+        st.write("")
         mes_anterior_index = (datetime.now().month - 1) % 12
         mes_sel = st.selectbox("📅 Período:", meses, index=mes_anterior_index)
         st.write("")
